be7ca5f831030c813e59746ed4691122057f2d40
Sunny9899 <63901097+Sunny9899@users.noreply.github.com>
2022-04-14 22:11:16 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import {Errbound} from './components/errBoundary';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Errbound/>
+    </div>
+  );
+}
+
+export default App;
